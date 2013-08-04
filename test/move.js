9c9da86ebc5490a4a07f7f0d35f698dd0aa4b863
ajlopez <webmaster@ajlopez.com>
2013-08-04 19:04:38 -0300

    Rebound left wall

--- test/move.js
+++ test/move.js
@@ -11,3 +11,13 @@ assert.equal(particle1.x, 11);
 assert.equal(particle1.y, 19);
 assert.equal(particle1.dx, 1);
 assert.equal(particle1.dy, -1);
+
+// rebound particle againts left wall
+
+var particle2 = p.createParticle(0, 20, -1, -1);
+
+particle2.move({ box: { left: 0, top: 0, right: 10, bottom: 30 } });
+assert.equal(particle2.x, 1);
+assert.equal(particle2.y, 19);
+assert.equal(particle2.dx, 1);
+assert.equal(particle2.dy, -1);
