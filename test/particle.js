f9b06dde84da54107813507ff56ada8dc2d08081
ajlopez <webmaster@ajlopez.com>
2013-08-04 18:55:50 -0300

    Additional test: create particle with velocity

--- test/particle.js
+++ test/particle.js
@@ -11,3 +11,22 @@ assert.equal(particle1.y, 20);
 assert.equal(particle1.dx, 0);
 assert.equal(particle1.dy, 0);
 
+// create particle without parameters
+
+var particle2 = p.createParticle();
+assert.ok(particle2);
+assert.equal(particle2.x, 0);
+assert.equal(particle2.y, 0);
+assert.equal(particle2.dx, 0);
+assert.equal(particle2.dy, 0);
+
+// create particle with velocity
+
+var particle3 = p.createParticle(10, 20, 1, -1);
+assert.ok(particle3);
+assert.equal(particle3.x, 10);
+assert.equal(particle3.y, 20);
+assert.equal(particle3.dx, 1);
+assert.equal(particle3.dy, -1);
+
+
